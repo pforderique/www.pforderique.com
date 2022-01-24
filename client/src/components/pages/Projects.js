26c94647a47a11174b1240c691ed0979e5fa54f5
pforderique <porderiq@mit.edu>
2022-01-24 18:07:01 -0500

    working projects page when no projects to display

--- client/src/components/pages/Projects.js
+++ client/src/components/pages/Projects.js
@@ -6,15 +6,24 @@ import ProjectGrid from "../modules/ProjectGrid";
 import "../../utilities.css";
 
 const Projects = (props) => {
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState(undefined);
 
   useEffect(() => {
-    get("/api/projects", { q: props.searchtext }).then((res) =>
-      setProjects(res)
-    );
+    get("/api/projects", { q: props.searchtext }).then((res) => {
+      setProjects(res);
+    });
   }, []);
 
-  if (projects.length === 0) return <Loading />;
+  if (projects === undefined) return <Loading />;
+
+  if (projects.length === 0)
+    return (
+      <div className="u-main-container u-textCenter">
+        No projects found. Try a different query. Click{" "}
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">here</a> for a
+        polynomial-time algorithm of the Halting Problem
+      </div>
+    );
 
   return (
     <>
