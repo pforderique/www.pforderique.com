d8a217102aac8bdb0955eec7c31429382023a7a3
pforderique <porderiq@mit.edu>
2022-01-26 15:50:05 -0500

    working loading module and /secret page

--- client/src/components/modules/Loading.js
+++ client/src/components/modules/Loading.js
@@ -1,13 +1,68 @@
 import React from "react";
+import * as THREE from "three";
 
 import "../../utilities.css";
 
-const Loading = (props) => {
-  return (
-    <>
-      <div className="u-textCenter">Loading page...</div>
-    </>
-  );
-};
+class Loading extends React.Component {
+  componentDidMount() {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    scene.background = new THREE.Color(0x031227);
+    camera.position.z = 100;
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    this.mount.appendChild(renderer.domElement);
+
+    const geometry = new THREE.TorusGeometry(6, 1.2, 6, 6);
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x64ffda,
+      // wireframe: true,
+    });
+    const torus = new THREE.Mesh(geometry, material);
+    torus.position.x -= 1;
+    torus.position.y += 25;
+    scene.add(torus);
+
+    const ambient = new THREE.AmbientLight(0xffffff);
+    scene.add(ambient);
+
+    var animate = function () {
+      requestAnimationFrame(animate);
+
+      // torus.rotation.x += 0.01;
+      torus.rotation.y += 0.08;
+      torus.rotation.z += 0.01;
+
+      renderer.render(scene, camera);
+    };
+    animate();
+  }
+  render() {
+    return (
+      <>
+        <div
+          id="info"
+          style={{
+            position: "absolute",
+            top: "52vh",
+            width: "100%",
+            textAlign: "center",
+            display: "block",
+          }}
+        >
+          Loading page
+        </div>
+        {/* <div className="u-textCenter u-highlight">Loading page...</div> */}
+        <div ref={(ref) => (this.mount = ref)} />
+      </>
+    );
+  }
+}
 
 export default Loading;
--- client/src/components/pages/Secret.js
+++ client/src/components/pages/Secret.js
@@ -1,10 +1,28 @@
-import React, { useEffect } from "react";
+import React from "react";
+import Loading from "../modules/Loading";
 
-const Secret = () => {
-  useEffect(() => {
-    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-  }, []);
-  return <div className="u-transparent u-textCenter">im so sorry</div>;
-};
+import "../../utilities.css";
+
+class Secret extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { canRedirect: false };
+  }
+
+  componentDidMount() {
+    const timer = setTimeout(() => {
+      this.setState({ canRedirect: true });
+      window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    }, 2000);
+    return () => clearTimeout(timer);
+  }
+  render() {
+    if (!this.state.canRedirect) {
+      return <Loading />;
+    }
+
+    return <div className="u-textCenter">im so sorry {this.state.counter}</div>;
+  }
+}
 
 export default Secret;
