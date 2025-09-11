3f88cb1c6b84ad456ab1ea98169ba90ddc011629
pforderique <pforderique@gmail.com>
2025-09-10 21:22:45 -0700

    remove mongodb models

--- server/models/resume.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ResumeSchema = new mongoose.Schema({
-  driveid: String,
-  date: Date,
-});
-
-// compile model from schema
-module.exports = mongoose.model("resume", ResumeSchema);
--- server/models/visitor.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const VisitorSchema = new mongoose.Schema({
-  number: Number,
-  date: Date,
-});
-
-// compile model from schema
-module.exports = mongoose.model("visitor", VisitorSchema);
