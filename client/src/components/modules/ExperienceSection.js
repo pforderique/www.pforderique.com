f7bd4cadcef3e363cd951b8a2d257fc8120d973d
pforderique <pforderique@gmail.com>
2025-09-16 20:03:02 -0700

    Remove old image fetching logic

--- client/src/components/modules/ExperienceSection.js
+++ client/src/components/modules/ExperienceSection.js
@@ -1,13 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 import Experience from "./Experience";
-// import borinquenfoods from "../../public/images/companyimages/borinquenfoods.png";
-// import mathnasium from "../../public/images/companyimages/mathnasium.png";
-// import microsoft from "../../public/images/companyimages/microsoft.png";
-// import mitos from "../../public/images/companyimages/mitos.png";
-// import nvidia from "../../public/images/companyimages/nvidia.png";
-// import google from "../../public/images/companyimages/google.png";
-
 import { get } from "../../utilities.js";
 import "./ExperienceSection.css";
 
@@ -16,15 +9,6 @@ import "./ExperienceSection.css";
  */
 const ExperienceSection = () => {
   const [experiences, setExperiences] = useState(undefined);
-
-  // const images = {
-  //   _borinquenfoods: borinquenfoods,
-  //   _mathnasium: mathnasium,
-  //   _mitos: mitos,
-  //   _microsoft: microsoft,
-  //   _nvidia: nvidia,
-  //   _google: google,
-  // };
 
   useEffect(() => {
     get("/api/experiences").then((exp) => setExperiences(exp));
